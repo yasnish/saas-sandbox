5435857420af2c31778bcfe757ccbcf88c679808
nantan <yasuki.nishinaka@gmail.com>
2024-10-03 18:14:19 +0900

    Add abount and contact pages

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md">
+        <h1 className="text-3xl font-bold mb-4 text-center">About Page</h1>
+        <p className="text-gray-700 text-center">This is the about page.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
