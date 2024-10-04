b354bda80ebe7f57a183848a6d83e40c25a8873e
nantan <yasuki.nishinaka@gmail.com>
2024-10-04 17:43:04 +0900

    Add Vol.71

--- app/contact/page.stories.tsx
+++ app/contact/page.stories.tsx
@@ -153,3 +153,5 @@ export const Vol68: Story = { args: { vol: 68 } };
 export const Vol69: Story = { args: { vol: 69 } };
 
 export const Vol70: Story = { args: { vol: 70 } };
+
+export const Vol71: Story = { args: { vol: 71 } };
