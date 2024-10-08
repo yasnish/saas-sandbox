7738dc690e89df4c58bfa39e6aeb1379d86bdcc8
nantan <yasuki.nishinaka@gmail.com>
2024-10-08 16:28:01 +0900

    Add modes

---
+++ .storybook/modes.ts
@@ -0,0 +1,27 @@
+export const allModes = {
+    mobile: {
+      viewport: "small",
+    },
+    desktop: {
+      viewport: "large",
+    },
+    dark: {
+      backgrounds: { value: "#1E293B" },
+      theme: "dark",
+    },
+    light: {
+      backgrounds: { value: "#fff" },
+      theme: "light",
+    },
+    "dark desktop": {
+      backgrounds: { value: "#1E293B" },
+      theme: "dark",
+      viewport: "large",
+    },
+    "light mobile": {
+      backgrounds: { value: "#fff" },
+      theme: "light",
+      viewport: "small",
+    },
+  };
+  
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,15 @@
 import type { Preview } from "@storybook/react";
 import "../app/globals.css";
+import { allModes } from "../.storybook/modes";
 
 const preview: Preview = {
   parameters: {
+    chromatic: {
+      modes: {
+        light: allModes["light"],
+        dark: allModes["dark"],
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
